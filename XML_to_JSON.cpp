5ceb9622306d7b9f63c324daa76a5edbcd94dd9c
Nouran-Salah <144157011+Nouran-Salah@users.noreply.github.com>
2024-12-22 21:49:53 +0300

    Update XML_to_JSON.cpp

--- XML_to_JSON.cpp
+++ XML_to_JSON.cpp
@@ -118,3 +118,60 @@ Node* parseXML(const std::string& xml) {
 
     return root;
 }
+
+
+void printJsonTree(const Node* node, int level, std::string& jsonBuilder, bool is_multilevel) {
+    std::string indentation = "    ";
+    std::string indent = std::string(level, indentation[0]);
+
+    jsonBuilder += indent;
+
+    if (!is_multilevel) {
+        jsonBuilder += "\"" + node->getTagName() + "\": ";
+    }
+
+    if (node->children.empty()) {
+        jsonBuilder += "\"" + node->getTagValue() + "\"";
+        return;
+    }
+
+    bool many_children_and_same_name = (node->children.size() > 1) &&
+        (node->children[0]->getTagName() == node->children[1]->getTagName());
+
+    if (!many_children_and_same_name) {
+        jsonBuilder += "{\n";
+        for (size_t i = 0; i < node->children.size(); ++i) {
+            printJsonTree(node->children[i], level + 1, jsonBuilder, false);
+            if (i < node->children.size() - 1) {
+                jsonBuilder += ",";
+            }
+            jsonBuilder += "\n";
+        }
+        jsonBuilder += indent + "}";
+        return;
+    }
+
+    jsonBuilder += "{\n" + indent + indentation + "\"" + node->children[0]->getTagName() + "\": [\n";
+    for (size_t i = 0; i < node->children.size(); ++i) {
+        printJsonTree(node->children[i], level + 2, jsonBuilder, true);
+        if (i < node->children.size() - 1) {
+            jsonBuilder += ",";
+        }
+        jsonBuilder += "\n";
+    }
+    jsonBuilder += indent + indentation + "]\n" + indent + "}";
+}
+
+void convertXMLToJSON(const std::string& xml) {
+    std::string xml_string = xmlString(xml);
+    Node* root = parseXML(xml_string);
+
+    std::string jsonString;
+    printJsonTree(root, 1, jsonString, false);
+
+    std::cout << "{\n";
+    std::cout << jsonString << std::endl;
+    std::cout << "}\n";
+}
+
+
