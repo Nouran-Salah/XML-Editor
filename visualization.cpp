6ec39c0281a6ba526cab18713b5a340b10f7223f
Amr Elmowaled <amrelmowaled@Amrs-MacBook-Pro.local>
2024-12-24 17:39:54 +0200

    adding graph visualization feature using graphviz

---
+++ visualization.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include "file_processing.cpp"
+
+int visualize_graph(const std::map<int, std::vector<int>>& graph) {
+    std::stringstream structure;
+
+    for(auto& g : graph) {
+        int target_user = g.first;
+        std::vector<int> followers = g.second;
+
+        structure << target_user << "\n";
+        for(int follower : followers) {
+            structure << follower << " -> " << target_user << "\n";
+        }
+    }
+    
+    write_file("digraph {\n" + structure.str() + "}", "graph.gv");
+    
+    return system("dot -Tpng graph.gv -o graph.png");
+}
+
