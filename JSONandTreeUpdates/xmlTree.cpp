2f3f7715f96c9541abca79fdf09a47f0de8a8f40
MariamAmrE <130848939+MariamAmrE@users.noreply.github.com>
2024-12-30 02:24:03 +0200

    Create xmlTree.cpp

---
+++ JSONandTreeUpdates/xmlTree.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include "xml_tree.h"
+
+
+Node::Node(const std::string& tagName) : tagName(tagName), parent(nullptr) {}
+
+void Node::addChild(Node* child) {
+    children.push_back(child);
+    child->parent = this;
+}
+
+std::string Node::getTagName() const { return tagName; }
+std::string Node::getTagValue() const { return tagValue; }
+
+//updated function
+string extractTagName(const string& line) {
+    size_t start = line.find('<');
+    size_t end = line.find('>');
+    if (start != string::npos && end != string::npos && end > start + 1) {
+        return line.substr(start + 1, end - start - 1);
+    }
+    return "";
+}
+
+//updated function
+string extractTagValue(const string& line, const string& tagName) {
+    string startTag = "<" + tagName + ">";
+    string endTag = "</" + tagName + ">";
+    size_t start = line.find(startTag);
+    size_t end = line.find(endTag);
+    if (start != string::npos && end != string::npos) {
+        return line.substr(start + startTag.length(), end - start - startTag.length());
+    }
+    return "";
+}
+
+
+Node* parseXML(const std::string& xml) {
+    Node* root = nullptr;
+    Node* currentNode = nullptr;
+    std::string currentText;
+
+    std::istringstream iss(xml);
+    std::string line;
+
+    while (std::getline(iss, line)) {
+        if (line.find('<') != std::string::npos) {
+            std::string tagName = extractTagName(line);
+
+            if (tagName.empty()) {
+                continue;
+            }
+
+            if (tagName.front() == '/') {
+                if (currentNode) {
+                    currentNode->tagValue = currentText;
+                    currentNode = currentNode->parent;
+                }
+            }
+            else {
+                if (line.find('>') != line.rfind('>')) {
+                    std::string tagValue = extractTagValue(line, tagName);
+                    Node* newNode = new Node(tagName);
+                    newNode->tagValue = tagValue;
+                    if (currentNode) {
+                        currentNode->addChild(newNode);
+                    }
+                    else {
+                        root = newNode;
+                    }
+                }
+                else {
+                    Node* newNode = new Node(tagName);
+                    if (currentNode) {
+                        currentNode->addChild(newNode);
+                    }
+                    else {
+                        root = newNode;
+                    }
+                    currentNode = newNode;
+                }
+            }
+            currentText = "";
+        }
+        else {
+            currentText += line + "\n";
+        }
+    }
+
+    return root;
+}
